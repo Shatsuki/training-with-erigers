a5f1ca64963837326f5a0c13d11bcf3e244252e2
DESKTOP-I2RBLVC\Shatsuki <ani12423@yahoo.gr>
2020-11-03 18:20:41 +0100

    add exercise 2 to git history

---
+++ exercise2/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="main">
+    <h1>hi</h1>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style>
+body {
+  width: 100vw;
+  height: 100vh;
+  background-color: grey;
+}
+#main {
+  width: 850px;
+  height: 850px;
+  background-color: rgb(20, 20, 20);
+}
+</style>
