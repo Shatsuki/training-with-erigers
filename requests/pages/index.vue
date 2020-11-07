16a57d148d31e2775c77d1f73fb2da280fb62f25
DESKTOP-I2RBLVC\Shatsuki <ani12423@yahoo.gr>
2020-11-07 13:52:13 +0100

    adding http requesting project

---
+++ requests/pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <pre>{{ ip }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData(context) {
+    console.log(context)
+    const ip = await context.$axios.$get(
+      'https://jsonplaceholder.typicode.com/todos'
+    )
+    return { ip }
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
